d1b8ceb018efcb3e99ddfb97723b3d6da903f443
dcolumbus2470 <156682238+dcolumbus2470@users.noreply.github.com>
2024-01-16 14:28:13 -0500

    added music

--- main.ts
+++ main.ts
@@ -2,9 +2,11 @@ let position = -90
 basic.forever(function () {
     if (fwdSensors.solar1.lightLevel() > 90) {
         basic.showIcon(IconNames.Heart)
+        music.play(music.tonePlayable(262, music.beat(BeatFraction.Whole)), music.PlaybackMode.LoopingInBackground)
         fwdMotors.leftServo.fwdSetEnabled(false)
     } else {
         basic.showIcon(IconNames.No)
+        music.play(music.stringPlayable("B A G A B B A A ", 120), music.PlaybackMode.LoopingInBackground)
         position += 10
         if (position > 90) {
             position = -90
